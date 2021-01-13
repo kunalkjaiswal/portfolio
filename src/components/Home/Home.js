08768f1fa6877de33738245ac865b0c264c26c0c
Kunal Jaiswal <contact.kunal13@gmail.com>
2021-01-14 03:14:46 +0530

    fix lanscape resolution issue

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -43,8 +43,7 @@ export default class Home extends Component {
                         <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
                         <li><a className="smoothscroll" href="#about">About</a></li>
                         <li><a className="smoothscroll" href="#skills">Skills</a></li>
-                        <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-                        <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+                        <li><a className="smoothscroll" href="#experience">Experience</a></li>
                         <li><a className="smoothscroll" href="#contact">Contact</a></li>
                     </ul>
 
